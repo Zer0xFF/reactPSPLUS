a72d4bfbbe6e5ea8914f52a9e35e3fb02bf06197
Mahmood(Thunder07) <m.a.hot.m+git@gmail.com>
2018-05-31 14:02:38 +0300

    add Notes

--- source/main.c
+++ source/main.c
@@ -11,7 +11,10 @@ uint64_t __readmsr(unsigned long __register)
 void resetTime()
 {
 	uint8_t* kernel_base = (uint8_t*)(__readmsr(0xC0000082) - 0x1C0);
-	// (?) sceSblSrtcReset
+	// Note args type (uint64_t*) could be incorrect (aka uint64_t or uint32_t) but im too lazy to check since it works
+	// Note somewhere between 1.76 and 5.05 `sceSblSrtcSetTime()` was changed and few extra checks have been added using samu
+	// So if you're backporting this you may not need or even find `sceSblSrtcClearTimeDifference()`
+	// Finally `sceSblSrtcClearTimeDifference()` is also named `sceSblSrtcReset()` when the 1st argument is 15
 	void(*sceSblSrtcClearTimeDifference)(uint64_t) = (void*)(kernel_base + 0x634690);
 	void(*sceSblSrtcSetTime)(uint64_t*) = (void*)(kernel_base + 0x634090);
 	sceSblSrtcClearTimeDifference(15);
